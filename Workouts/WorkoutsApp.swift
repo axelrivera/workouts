15f010e15e7388161d000d91e73676e3bcf74f80
Axel Rivera <axel@axelrivera.me>
2021-02-24 08:14:31 -0500

    feat: Add debug flag and error to prevent overriding production release

--- Workouts/WorkoutsApp.swift
+++ Workouts/WorkoutsApp.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+// Don't install Production Debug version on device to prevent from overriding an installed production app
+// Can be commented if needed
+
+#if PRODUCTION_DEBUG
+#error("preventing debug version from overriding production release")
+#endif
+
 @main
 struct WorkoutsApp: App {
     let workoutManager = WorkoutManager()
