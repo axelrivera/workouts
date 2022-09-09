f5d119f2a72a0310cf15ab93bfb5bbf44c399a4d
Axel Rivera <axel@axelrivera.me>
2022-09-09 04:50:57 -0400

    wip: add weather provider file

--- Workouts/WeatherProvider.swift
+++ Workouts/WeatherProvider.swift
@@ -7,7 +7,24 @@
 
 import Foundation
 import WeatherKit
+import CoreLocation
 
-struct WeatherProvider {
-    
-}
+//@MainActor
+//class WeatherProvider: ObservableObject {
+//
+//    static var isWeatherAvailable: Bool {
+//        if #available(iOS 16.0, *) {
+//            return true
+//        } else {
+//            return false
+//        }
+//    }
+//
+//    @available(iOS 16.0, *)
+//    func weather(forLocation location: CLLocation, start: Date, end: Date) async {
+//        let service = WeatherService.shared
+//
+//
+//    }
+//
+//}
